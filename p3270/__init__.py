fea13bfc90e2333585b2dab54fdf936660388142
Mossaab Stiri <mossaab.stiri@gmail.com>
2017-10-26 23:51:57 +0100

    Fix __init__.py import and setup.py for new release

---
+++ p3270/__init__.py
@@ -0,0 +1,1 @@
+from p3270.p3270 import S3270, P3270Client, Config, StatusMessage
--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from distutils.core import setup
 setup(
         name = 'p3270',
         packages = ['p3270'],
-        version = '0.1.0',
+        version = '0.1.1',
         description = 'Python library to communicate with IBM hosts',
         author = 'Mossaab Stiri',
         author_email = 'mossaab.stiri@gmail.com',
         url = 'https://github.com/mstiri/p3270',
         long_description = '''
-    A Python library to acces IBM hosts. The library provides an interface to communicate with IBM hosts: send commands and text, receive output (screens). The library provides the means to do what a human can do using a 3270 emulator.
+    A Python library that provides an interface to communicate with IBM hosts: send commands and text, receive output (screens). The library provides the means to do what a human can do using a 3270 emulator.
 
     The library is highly customizable and is built with simplicity in mind.
     It is written in Python 3, runs on Linux and Unix-like Operating Systems, and relies on the `s3270` utility. So it is required to have the `s3270` installed on your system and available on your PATH.
 
     The library allows you to open a telnet connection to an IBM host, and execute a set of instructions as you specified them in your python program.
     ''',
-        keywords = ['IBM', 'CICS', '3270', 'TN3270', 'automation', 'Mainframe z/OS'],
+        keywords = 'IBM CICS 3270 TN3270 test automation Mainframe z/OS',
         classifiers = [
             'Intended Audience :: Developers',
             'Programming Language :: Python',
@@ -27,6 +27,6 @@ setup(
             'Topic :: Software Development :: Testing',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Development Status :: 5 - Production/Stable'
+            'Development Status :: 4 - Beta'
         ]
 )
