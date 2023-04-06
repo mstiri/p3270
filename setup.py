e11c36a1d631ae534a83d48a5ab7c3d39de5cd8f
Mossaab Stiri <mossaab.stiri@gmail.com>
2023-04-06 07:14:03 +0200

    Update setup version: 0.1.5

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 import setuptools
 
 setuptools.setup(
-        name = 'p3270',
-        packages = ['p3270'],
-        version = '0.1.3',
-        description = 'Python library to communicate with IBM hosts',
-        author = 'Mossaab Stiri',
-        author_email = 'mossaab.stiri@gmail.com',
-        url = 'https://github.com/mstiri/p3270',
-        long_description = '''
+    name='p3270',
+    packages=['p3270'],
+    version='0.1.5',
+    description='Python library to communicate with IBM hosts',
+    author='Mossaab Stiri',
+    author_email='mossaab.stiri@gmail.com',
+    url='https://github.com/mstiri/p3270',
+    long_description='''
     A Python library that provides an interface to communicate with IBM hosts: send commands and text, receive output (screens). The library provides the means to do what a human can do using a 3270 emulator.
 
     The library is highly customizable and is built with simplicity in mind.
@@ -17,17 +17,17 @@ setuptools.setup(
 
     The library allows you to open a telnet connection to an IBM host, and execute a set of instructions as you specified them in your python program.
     ''',
-        keywords = 'IBM CICS 3270 TN3270 test automation Mainframe z/OS',
-        classifiers = [
-            'Intended Audience :: Developers',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3 :: Only',
-            'Operating System :: Unix',
-            'Operating System :: POSIX :: Linux',
-            'Topic :: Software Development :: Testing',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Development Status :: 4 - Beta'
-        ]
+    keywords='IBM CICS 3270 TN3270 test automation Mainframe z/OS',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Beta'
+    ]
 )
